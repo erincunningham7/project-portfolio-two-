9d6e47dc8acf1c491b1581a2ca586874390f6caa
erincunningham7 <101681089+erincunningham7@users.noreply.github.com>
2023-04-21 17:42:32 +0000

    Add quiz questions to script.js file

---
+++ assets/script.js
@@ -0,0 +1,83 @@
+// Quiz Questions
+const questions = [
+    {
+        question: 'How many kids does Angelina Jolie have?',
+        a: '8',
+        b: '6',
+        c: '10',
+        d: '5',
+        correct: 'b',
+    },
+    {
+        question: 'Who did Kim Kardashian call "the least interesting to look at"?',
+        a: 'Kylie',
+        b: 'Khloe',
+        c: 'Rob',
+        d: 'Kourtney',
+        correct: 'd',
+    },
+    {
+        question: 'Who played the role of Elvis Presley in the 2022 movie Elvis?',
+        a: 'Austin Butler',
+        b: 'Timothee Chalamet',
+        c: 'Leonardo Dicaprio',
+        d: 'Theo James',
+        correct: 'a',
+    },
+    {
+        question: 'Who was exposed for having a secret love child in a diss track by Pusha T in 2018?',
+        a: 'Tristan Thompson',
+        b: 'Robin Thicke',
+        c: 'Drake',
+        d: 'Kanye West',
+        correct: 'c',
+    },
+    {
+        question: 'Who played Tanya in season one and two of White Lotus?',
+        a: 'Zendaya',
+        b: 'Jennifer Coolidge',
+        c: 'Aubrey Plaza',
+        d: 'Connie Britton',
+        correct: 'b',
+    },
+    {
+        question: 'Real Housewives star Taylor Armstrong become a viral meme that depicted her screaming at what animal?',
+        a: 'Cat',
+        b: 'Turtle',
+        c: 'Seal',
+        d: 'Frog',
+        correct: 'a',
+    },
+    {
+        question: 'Which beauty Youtuber was infamously exposed in 2019 for selling mouldy lipsticks?',
+        a: 'James Charles',
+        b: 'Jeffree Star',
+        c: 'Kat Von D',
+        d: 'Jaclyn Hill',
+        correct: 'd',
+    },
+    {
+        question: 'What did Molly Mae and Tommy Fury name their baby girl?',
+        a: 'Halo',
+        b: 'Nephele',
+        c: 'Bambi',
+        d: 'Cloud',
+        correct: 'c',
+    },
+    {
+        question: 'When did Aaron ask Cady what day was it in Mean Girls?',
+        a: 'October 3',
+        b: 'March 29',
+        c: 'January 1',
+        d: 'May 4',
+        correct: 'a',
+    },
+    {
+        question: 'Which music video was the first to be played on MTV?',
+        a: 'Never Gonna Give You Up',
+        b: 'Walking On Sunshine',
+        c: 'Video Killed The Radio Star',
+        d: 'Highway To Hello',
+        correct: 'c'
+    }
+]
